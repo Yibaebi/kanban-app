f588d9a556b830473f35302e20db79c68034d116
yibaebi <elliot.yibaebi@yahoo.com>
2023-05-04 21:51:45 +0100

    configure sass, tailwind, and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import { Plus_Jakarta_Sans } from 'next/font/google'
+import { Plus_Jakarta_Sans } from 'next/font/google';
 
-const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${plusJakartaSans.className}`}>
-      Welcome to Kanban App
+    <main
+      className={`
+        flex min-h-screen flex-col items-center justify-center bg-blue-light p-24 dark:bg-dark
+        ${plusJakartaSans.className}
+      `}
+    >
+      <h2 className=" dark:text-white">Welcome to Kanban App</h2>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,27 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      sm: ['12px', '15px'],
+      base: ['15px', '19px'],
+      lg: ['18px', '23px'],
+      xl: ['24px', '30px'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        purple: '#635FC7',
+        'purple-light': '#A8A4FF',
+        black: '#000112',
+        dark: '#20212C',
+        gunmetal: '#2B2C37',
+        arsenic: '#3E3F4E',
+        blue: '#E4EBFA',
+        'blue-dark': '#828FA3',
+        'blue-light': '#F4F7FD',
+        'red-dark': '#EA5555',
+        'red-light': '#FF9898',
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
